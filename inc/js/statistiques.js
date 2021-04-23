8be565735fc9041b70d39135b2bac888ca754eb8
Warkild <alexandre17.jaud@gmail.com>
2021-04-23 13:12:49 +0200

    V.0.3
    [STATISTIQUES]
    > Ajout des statistiques des vêtements déjà enregistré

--- inc/js/statistiques.js
+++ inc/js/statistiques.js
@@ -1,15 +1,25 @@
 function init() {
     longueur_couvre_chefs = couvre_chef.length;
+    longueur_vetement = vetements.length;
 
     ref_couvre_chefs(longueur_couvre_chefs);
+    ref_vetement(longueur_vetement);
     quantite_couvre_chefs();
+    quantite_vetement(longueur_vetement);
     cartons_couvre_chefs();
+    cartons_vetement();
 }
 
 ///// Affichage nombres références couvre_chefs /////
 
 function ref_couvre_chefs(longueur_couvre_chefs) {
     document.getElementById("nb_ref_coiffes").innerHTML = longueur_couvre_chefs
+}
+
+///// Affichage nombres références vetement /////
+
+function ref_vetement(longueur_vetement) {
+    document.getElementById("nb_ref_vetement").innerHTML = longueur_vetement
 }
 
 ///// Affichage quantité couvre_chefs /////
@@ -21,6 +31,17 @@ function quantite_couvre_chefs() {
         quantite_couvre_chefs += (couvre_chef[i]['quantite'])
     }
     document.getElementById("nb_coiffes").innerHTML = quantite_couvre_chefs
+}
+
+///// Affichage quantité vetement /////
+
+function quantite_vetement(longueur_vetement) {
+    var quantite_vetement = 0;
+    var longueur = longueur_vetement
+    for (i=0; i<longueur; i++) {
+        quantite_vetement += (vetements[i]['quantite'])
+    }
+    document.getElementById("nb_vetement").innerHTML = quantite_vetement;
 }
 
 ///// Affichage cartons couvre_chefs /////
@@ -65,6 +86,51 @@ function cartons_couvre_chefs() {
         newDiv.className = "NomCartons";
         newDiv.innerHTML = collection_couvre_chef[i]['reference']
         document.getElementById('divGeneralBoxCartonsCouvreChefs' + i).appendChild(newDiv);
+    }
+}
+
+///// Affichage cartons vetement /////
+
+
+function cartons_vetement() {
+
+    // Création de la collection vetement
+
+    var collection_vetement = []
+
+    for (var i=0; i<cartons.length; i++) {
+        if(cartons[i]['type'] == "vetement") {
+            collection_vetement.push(cartons[i]);
+        }
+    }
+
+    // Création div général
+
+    for (i=0;i<collection_vetement.length;i++) {
+        var newDiv       = document.createElement("div");
+        newDiv.id        = "divGeneralBoxCartonsVetement" + (i);
+        newDiv.className = "divGeneralBox";
+        document.getElementById('vetement_cartons').appendChild(newDiv);
+    }
+
+    // Création infos nombre 
+
+    for (i=0;i<collection_vetement.length;i++) {
+        var newDiv       = document.createElement("p");
+        newDiv.id        = "NombreBoxCartonsVetement" + (i);
+        newDiv.className = "nombreCartons";
+        newDiv.innerHTML = collection_vetement[i]['nombre']
+        document.getElementById('divGeneralBoxCartonsVetement' + i).appendChild(newDiv);
+    }
+
+    // Création infos nom 
+
+    for (i=0;i<collection_vetement.length;i++) {
+        var newDiv       = document.createElement("p");
+        newDiv.id        = "NomBoxCartonsVetement" + (i);
+        newDiv.className = "NomCartons";
+        newDiv.innerHTML = collection_vetement[i]['reference']
+        document.getElementById('divGeneralBoxCartonsVetement' + i).appendChild(newDiv);
     }
 }
 
